631553b4bbac7926867aaecc109822d5cf08fc14
yomajo <ed.vinas@yahoo.com>
2020-09-29 17:47:44 +0300

    added labels sorting and export to txt; Deleted old, redundant code from previous repo

--- Helper Files/parse_orders.py
+++ Helper Files/parse_orders.py
@@ -1,5 +1,4 @@
 from amzn_parser_utils import get_output_dir
-# from orders_report import OrdersReport
 from collections import defaultdict
 from datetime import datetime
 import logging
@@ -12,98 +11,77 @@ import os
 VBA_ERROR_ALERT = 'ERROR_CALL_DADDY'
 VBA_NO_NEW_JOB = 'NO NEW JOB'
 VBA_KEYERROR_ALERT = 'ERROR_IN_SOURCE_HEADERS'
-DPOST_REF_CHARLIMIT_PER_CELL = 28
 
 
 class ParseOrders():
-    '''Input: orders as list of dicts, parses orders, groups, forms output object;
-    passes to OrdersReport class which creates report in xlsx format.
+    '''Input: orders as list of dicts, parses orders, forms sorted export obj;
+    exports helper txt file with custom labels and corresponding quantities
     Interacts with database client instance; main method:
     
-    export_orders(testing=False) : groups orders by EU/ non-EU orders, with nesting based on currency.    
-    when testing flag = True, export is suspended, but orders passed to class are still added to database'''
+    export_orders(testing=False)    NOTE: check behaviour when testing flag is True in export_orders'''
     
     def __init__(self, all_orders : list, db_client : object):
         self.all_orders = all_orders
         self.db_client = db_client
-        self.eu_orders = []
-        self.non_eu_orders = []
     
-    def _prepare_filepaths(self):
-        '''creates cls variables of files abs paths to be created one dir above this script dir'''
+    def _prepare_filepath(self):
+        '''constructs cls variable of output abs file path'''
         output_dir = get_output_dir()
         date_stamp = datetime.today().strftime("%Y.%m.%d %H.%M")
-        self.report_path = os.path.join(output_dir, f'Amazon Orders Report {date_stamp}.xlsx')
+        self.inventory_file = os.path.join(output_dir, f'Amazon Inventory Reduction {date_stamp}.txt')
     
-    def split_orders_by_tax_region(self):
-        '''splits all_orders into two lists: EU (VAT (item-tax) > 0) and NON-EU (VAT = 0); Exits if resulting lists are empty'''    
+    def get_orders_labels_obj(self) -> dict:
+        '''returns a dictionary/counter of unique codes in form: {'code1':5, 'code2':3, ... 'coden':2}'''    
+        self.labels = defaultdict(int)
         for order in self.all_orders:
             try:
-                if float(order['item-tax']) > 0:
-                    self.eu_orders.append(order)
-                else:
-                    self.non_eu_orders.append(order)
+                self.labels[order['sku']] += self.get_order_quantity(order)
             except KeyError:
-                logging.exception(f'Could not find item-tax in order keys. Order: {order}\nClosing connection to database, alerting VBA, exiting...')
+                logging.exception(f'Could not find \'sku\' in order keys. Order: {order}\nClosing connection to database, alerting VBA, exiting...')
                 self.db_client.close_connection()
                 print(VBA_KEYERROR_ALERT)
-                sys.exit()                
-            except ValueError:
-                logging.exception(f'Could not return float value for item-tax in order: {order}\nClosing connection to database, alerting VBA, exiting...')
-                self.db_client.close_connection()
-                print(VBA_ERROR_ALERT)
                 sys.exit()
         self.exit_no_new_orders()
+        return self._sort_labels(self.labels)
     
+    @staticmethod
+    def _sort_labels(labels:dict) -> list:
+        '''sorts default dict by descending quantities. Returns list of tuples'''
+        return sorted(labels.items(), key=lambda lab_qty_tuple: lab_qty_tuple[1], reverse=True)
+
+    def get_order_quantity(self, order:dict) -> int:
+        '''returns 'quantity-purchased' order key value in integer form'''
+        try:
+            return int(order['quantity-purchased'])
+        except ValueError:
+            logging.exception(f'Could convert quantity to integer. Order: {order}\nClosing connection to database, alerting VBA, exiting...')
+            self.db_client.close_connection()
+            print(VBA_ERROR_ALERT)
+            sys.exit()
+        except KeyError:
+            logging.exception(f'No such key \'quantity-purchased\'. Order: {order}\nClosing connection to database, alerting VBA, exiting...')
+            self.db_client.close_connection()
+            print(VBA_KEYERROR_ALERT)
+            sys.exit()
+
     def exit_no_new_orders(self):
-        if not self.eu_orders and not self.non_eu_orders:
+        '''Suspend program, warn VBA if no new orders were found'''
+        logging.info(f'INSIDE EXIT_NO_NEW_ORDERS. Length of self.labels: {len(self.labels)}. type validation: {type(self.labels)}')
+        if len(self.labels) == 0:
             logging.info(f'No new orders found. Terminating, closing database connection, alerting VBA.')
             self.db_client.close_connection()
             print(VBA_NO_NEW_JOB)
             sys.exit()
 
-    def prepare_export_obj(self):
-        '''constructs a dict data object for OrdersReport class. Output format:
-        export_data = {
-                eu_orders: {
-                            currency1: [order1, order2, order...],
-                            currency2: [order1, order2, order...],
-                            currency_n : [order1, order2, order...]
-                            },
-                non_eu_orders: {
-                            currency1: [order1, order2, order...],
-                            currency2: [order1, order2, order...],
-                            currency_n : [order1, order2, order...]
-                                }
-                    }'''
-        eu_currency_grouped = self.get_region_currency_based_dict(self.eu_orders)
-        non_eu_currency_grouped = self.get_region_currency_based_dict(self.non_eu_orders)
-        self.export_obj = {'EU' : eu_currency_grouped, 'NON-EU' : non_eu_currency_grouped}
-        return self.export_obj
-
-    @staticmethod
-    def get_region_currency_based_dict(region_orders : list) -> dict:
-        currency_based_dict = defaultdict(list)
-        for order in region_orders:
-            currency_based_dict[order['currency']].append(order)
-        return currency_based_dict
-
-    def export_report(self):
-        '''creates OrdersReport instance, and exports report in xlsx format'''
-        try:
-            self.export_labels(self.export_obj)
-            # OrdersReport(self.export_obj).export(self.report_path)
-            # logging.info(f'XLSX report {os.path.basename(self.report_path)} successfully created.')
-        except:
-            logging.exception(f'Unexpected error creating report. Closing database connection, alerting VBA, exiting...')
-            self.db_client.close_connection()
-            print(VBA_ERROR_ALERT)
-            sys.exit()
-    
-    def export_labels(self, labels:dict):
-        '''outputs a helper txt file to reduce inventory'''
-        pass
-    
+    def export_inventory_helper_file(self):
+        '''exports txt file with sorted custom labels (sku's) by quantity'''
+        export_labels = self.get_orders_labels_obj()
+        with open(self.inventory_file, 'w', encoding='utf-8') as f:
+            for label, qty in export_labels:
+                line = f'{label:<20} {qty:>4}\n'
+                f.write(line)
+        logging.info(f'Export completed! TXT inventory helper file {os.path.basename(self.inventory_file)} successfully created.')
+        
     def push_orders_to_db(self):
         '''adds all orders in this class to orders table in db'''
         count_added_to_db = self.db_client.add_orders_to_db()
@@ -111,18 +89,15 @@ class ParseOrders():
 
     def export_orders(self, testing=False):
         '''Summing up tasks inside ParseOrders class'''
-        self._prepare_filepaths()
-        self.split_orders_by_tax_region()
-        self.prepare_export_obj()        
+        self._prepare_filepath()
         if testing:
             logging.info(f'Due to flag testing value: {testing}. Order export and adding to database suspended. Change behaviour in export_orders method in ParseOrders class')
             print(f'Due to flag testing value: {testing}. Order export and adding to database suspended. Change behaviour in export_orders method in ParseOrders class')
-            self.db_client.close_connection()
             print('ENABLED REPORT EXPORT WHILE TESTING')
-            self.export_report()
-            # self.push_orders_to_db()
+            self.export_inventory_helper_file()
+            self.push_orders_to_db()
             return
-        self.export_report()
+        self.export_inventory_helper_file()
         self.push_orders_to_db()
 
 if __name__ == "__main__":
