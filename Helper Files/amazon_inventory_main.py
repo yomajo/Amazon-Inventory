c983e76cc617f3446a721e790f7384dae18948e3
yomajo <ed.vinas@yahoo.com>
2020-11-12 18:58:32 +0200

    del some meaningless msgs, reset vars to procution

--- Helper Files/amazon_inventory_main.py
+++ Helper Files/amazon_inventory_main.py
@@ -9,19 +9,19 @@ import os
 
 
 # GLOBAL VARIABLES
-TESTING = True
+TESTING = False
 EXPECTED_SYS_ARGS = 2
 VBA_ERROR_ALERT = 'ERROR_CALL_DADDY'
 VBA_KEYERROR_ALERT = 'ERROR_IN_SOURCE_HEADERS'
 VBA_OK = 'EXPORTED_SUCCESSFULLY'
 if is_windows_machine():
-    TEST_AMZN_EXPORT_TXT = r'C:\Coding\Ebay\Working\Backups\Amazon exports\Collected exports\export 2020.06.16.txt'
+    TEST_AMZN_EXPORT_TXT = r'C:\Coding\Ebay\Working\Backups\Amazon exports\Collected exports\export 2020.06.17.txt'
 else:
     TEST_AMZN_EXPORT_TXT = r'/home/devyo/Coding/Git/Amazon Inventory/Amazon exports/run1.txt'
 
 # Logging config:
 log_path = os.path.join(get_output_dir(client_file=False), 'amazon_inventory.log')
-logging.basicConfig(handlers=[logging.FileHandler(log_path, 'a', 'utf-8')], level=logging.DEBUG)
+logging.basicConfig(handlers=[logging.FileHandler(log_path, 'a', 'utf-8')], level=logging.INFO)
 
 
 def get_cleaned_orders(source_file:str) -> list:
@@ -75,8 +75,6 @@ def main(testing, amazon_export_txt_path):
         print('RUNNING IN TESTING MODE')
         txt_path = amazon_export_txt_path
     if os.path.exists(txt_path):
-        logging.info('file exists, continuing to processing...')
-        print(f'File {os.path.basename(txt_path)} exists')
         cleaned_source_orders = get_cleaned_orders(txt_path)
         parse_export_orders(testing, cleaned_source_orders, txt_path)
         print(VBA_OK)
